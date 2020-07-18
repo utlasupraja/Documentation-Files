30fd5b81f5257bad3b11f1821f1e67b2c037d373
utlasupraja <68277488+utlasupraja@users.noreply.github.com>
2020-07-18 14:32:05 +0530

    Update conf.py

--- conf.py
+++ conf.py
@@ -17,9 +17,9 @@
 
 # -- Project information -----------------------------------------------------
 
-project = 'Web Design and Development - Apssdc'
-copyright = '2020, APSSDC'
-author = 'APSSDC'
+project = 'Attendance Management System'
+copyright = '2020, Sup'
+author = 'Sup'
 
 # The full version, including alpha/beta/rc tags
 release = '1'
